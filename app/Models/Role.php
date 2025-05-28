915ef849e7d56e092a0e386429fe1ac51ce9417b
Zhal235 <Zhal235@gmail.com>
2025-05-29 00:29:06 +0700

    `Added $fillable array and two getter methods to Role model`

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,10 +6,33 @@ use Spatie\Permission\Models\Role as SpatieRole;
 
 class Role extends SpatieRole
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'name',
         'guard_name',
     ];
-{
-    //
+
+    /**
+     * Get the guard name for the role.
+     *
+     * @return string
+     */
+    public function getGuardName()
+    {
+        return $this->guard_name;
+    }
+
+    /**
+     * Get the role name.
+     *
+     * @return string
+     */
+    public function getRoleName()
+    {
+        return $this->name;
+    }
 }
